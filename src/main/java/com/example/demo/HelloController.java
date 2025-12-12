27a98fd2faad636e9073f2ec9e4cb7d22eac6aad
Khandamir <dameer.db@gmail.com>
2025-12-12 18:34:35 +0500

    Commit test

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello() {
-            return "Hello from Java Backend v2!";
+            return "Hello from Java Backend v236!";
 
     }
 }
