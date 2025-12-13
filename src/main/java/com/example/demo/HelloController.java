48d91e93c20e3efc2ad4f479a7f0983b7745545d
✨Divine_Intervention✨ <yesmukhamedov.yeskendyr@gmail.com>
2025-12-13 10:31:26 +0500

    feat: v5

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello() {
-            return "Hello from Java Backend v4!";
+            return "Hello from Java Backend v5!";
 
     }
 }
