e0498673c98f08a3aac32a913560dca53474be7c
Khandamir <dameer.db@gmail.com>
2025-12-15 12:24:41 +0500

    FEAT 16

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
@@ -11,5 +13,6 @@ public class HelloController {
         return "backend-demo OK";
     }
 }
+
 
 
