7920d5b4a4cbe79118b8dc4b7eeeb213d1c0d2d3
Khandamir <dameer.db@gmail.com>
2025-12-15 12:01:14 +0500

    FEAT 15

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public String hello() {
-            return "backend-demo version: v1.0.3 (commit df40b6)";
-
+        return "backend-demo version: v1.0.3 (commit df40b6)";
     }
 }
+
